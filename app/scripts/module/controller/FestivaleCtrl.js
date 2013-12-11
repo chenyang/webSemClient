448aa9fafb428d8a37ea1b5d40a7f43c145ee1d6
dellgcy <chenyanggao123@gmail.com>
2013-12-11 23:04:25 +0100

    affichage en utilisant ng-grid pour les evenements

--- app/scripts/module/controller/FestivaleCtrl.js
+++ app/scripts/module/controller/FestivaleCtrl.js
@@ -2,9 +2,52 @@
 	'use strict';
 	var module = angular.module('mod.controller');
 
-	module.controller('FestivaleCtrl', ['$scope', 'cmWSFacade', function($scope, cmWSFacade){
-		
-		
+	module.controller('FestivaleCtrl', ['$scope', 'cmWSFacade', 'webStorage', 'EvenementService',
+	                                    function($scope, cmWSFacade, webStorage,EvenementService){
+
+		//Get position $info_geo
+		$scope.longitude = webStorage.session.get('$info_geo').longitude; 
+		$scope.latitude = webStorage.session.get('$info_geo').latitude; 
+		$scope.$info_user = webStorage.session.get('$info_user');
+
+		$scope.dataEvenements = [];
+
+		$scope.gridOptions = {
+				data: 'dataEvenements',
+				i18n:'fr',
+				enableColumnResize:true, 
+				enableRowReordering:false, 
+				enableSorting:true,
+				showColumnMenu:true, 
+				showFilter:true,
+				showSelectionCheckbox:false, 
+				selectWithCheckboxOnly:false,
+				//plugins:[new ngGridFlexibleHeightPlugin()],
+				columnDefs: [{field:'nom', displayName:'Nom Festival'}, 
+				             {field:'date', displayName:'Date'},
+				             {field:'genre', displayName:'Genre de musique'}, 
+				             {field:'participant', displayName:'Artists Participants'},
+				             {field:'wikilink', displayName:'WikiLink', cellTemplate:'template/templateArtist.html'}, 
+				             ]
+		}
+
+
+
+		$scope.init = function(){
+			//Get Liste styles from user
+			EvenementService.getAllEvenements($scope.latitude, $scope.longitude, 50).success(function(data, status){
+				$scope.dataEvenements = data.binding;
+				console.log($scope.dataEvenements);
+			});
+		}
+
+
+		//Methode a initialiser
+		$scope.init();
+
+
+
+
 	}]);
 
 })();
